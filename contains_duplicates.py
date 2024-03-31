852b7715f28b39e98d0bf81a504d0ab124fb08fa
Philip <macpilot23@outlook.com>
2024-03-31 18:02:59 +0100

    contains duplicates

---
+++ contains_duplicates.py
@@ -0,0 +1,21 @@
+# Topics: Array, Hash Table, Sorting
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        checked_numbers = set()
+        for number in nums:
+            if number not in checked_numbers:
+                checked_numbers.add(number)
+            else:
+                return True
+        return False
+
+
+def main():
+    solution = Solution()
+    numbers = [1, 2, 3, 4, 5]
+    contains_duplicates = solution.containsDuplicate(numbers)
+    print(contains_duplicates)
+
+if __name__ == "__main__":
+    main()
