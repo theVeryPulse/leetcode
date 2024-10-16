470a78eefa11ac7765be794dd973ac6b894cd9ea
Philip <macpilot23@outlook.com>
2024-10-16 15:58:21 +0100

    feat (contains duplicates): add solutions in c++20

---
+++ 00_arrays_and_hashing/00_contains_duplicates/contains_duplicates_c++20.cpp
@@ -0,0 +1,37 @@
+/**
+ * @brief Given an integer array nums, return true if any value appears at least
+ *        twice in the array, and return false if every element is distinct.
+ *
+ * @ref   https://leetcode.com/problems/contains-duplicate/description/
+ */
+
+#include <vector>
+#include <unordered_set>
+
+class Solution
+{
+  public:
+    bool containsDuplicate(std::vector<int>& nums)
+    {
+        std::unordered_set<int> track;
+        track.reserve(nums.size());
+        for (const int& num : nums)
+        {
+            if (!track.insert(num).second)
+                return true;
+        }
+        return false;
+    }
+};
+
+#include <iostream>
+
+int main()
+{
+    // std::vector<int> vec = {1, 1, 3, 3, 4, 3, 2, 4, 2};
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+
+    Solution solution;
+    std::cout << "Contains duplicate: " << solution.containsDuplicate(vec)
+              << "\n";
+}
