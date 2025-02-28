117b6be62334b1238c510ff821729a25d7036400
Philip Li <macpilot23@outlook.com>
2025-02-28 12:16:20 +0000

    refactor (two sum): simplfy logic in python

--- 00_arrays_and_hashing/02_two_sum/two_sum.py
+++ 00_arrays_and_hashing/02_two_sum/two_sum.py
@@ -2,17 +2,11 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         num_index = dict()
         for index, num in enumerate(nums):
-            if num_index.get(num) is None:
-                num_index[num] = [index]
-            else:
-                num_index[num] += [index]
-
-        for index, num in enumerate(nums):
             if (target - num) in num_index:
-                if num * 2 == target and len(num_index[num]) > 1:
-                    return [index, num_index[num][1]]
-                elif num * 2 != target:
-                    return [index, num_index[target - num][0]]
+                return [num_index[target - num], index]
+            else:
+                num_index[num] = index
+        return []
 
 
 def main():
