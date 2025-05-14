34eeed7e3000b19e046fde7b50e2fbce8ffa16b2
Philip Li <macpilot23@outlook.com>
2025-05-14 23:53:44 +0100

    move folder

--- LeetCode75/array_and_string/kids_with_the_greatest_number_of_candies/normal.py
+++ LeetCode75/array_and_string/kids_with_the_greatest_number_of_candies/normal.py
@@ -3,7 +3,5 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         greatest = max(candies)
-        check = []
-        for candy_count in candies:
-            check.append(candy_count + extraCandies >= greatest)
+        check = [candy_count + extraCandies >= greatest for candy_count in candies]
         return check
